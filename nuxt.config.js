bff94c940c95c9d7cd66bc9f81ea1c929dbb0813
Alexander Danilov <alxx.danilov@gmail.com>
2020-12-31 00:31:18 +0300

    добавлен снипет для соц сетей

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,13 +8,14 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { hid: 'img', property:'og:image', content:'/img/facebook.png'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap'
-      }
+      },
     ],
 
   },
